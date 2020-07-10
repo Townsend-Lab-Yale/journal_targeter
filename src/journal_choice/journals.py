d2c986fa715efdf3baf1f35aa390e94c61b25dc3
Stephen Gaffney <stephen.gaffney@yale.edu>
2020-07-10 16:29:32 +0100

    Add deploy and develop CLI flask commands for setup.

--- src/journal_choice/journals.py
+++ src/journal_choice/journals.py
@@ -14,3 +14,23 @@ _logger = logging.getLogger(__name__)
 from .app import create_app
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+
+@app.cli.command()
+def deploy():
+    """Run deployment tasks."""
+    import shutil
+    from .demo import update_demo_plot
+    update_demo_plot(os.environ.get('DEMO_PREFIX', 'demo'))
+    use_env = '.env.server'
+    shutil.copy(use_env, '.env')
+    print(f"Copied from {use_env} to .env")
+
+
+@app.cli.command()
+def develop():
+    """Set up development server."""
+    import shutil
+    use_env = '.env.dev'
+    shutil.copy(use_env, '.env')
+    print(f"Copied from {use_env} to .env")
