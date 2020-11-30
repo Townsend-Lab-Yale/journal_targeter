ac5e29730396df6459476cd58606e73024d4cda8
Stephen Gaffney <stephen.gaffney@yale.edu>
2020-11-30 12:16:27 +0000

    Create demo and metrics dirs at startup.

--- src/journal_choice/__init__.py
+++ src/journal_choice/__init__.py
@@ -14,3 +14,5 @@ finally:
 DATA_DIR = os.path.sep.join(__file__.split(os.path.sep)[:-3] + ['data'])
 DEMO_DIR = os.path.join(DATA_DIR, 'demo')
 METRICS_DIR = os.path.join(DATA_DIR, 'metrics')
+os.makedirs(DEMO_DIR, exist_ok=True)
+os.makedirs(METRICS_DIR, exist_ok=True)
