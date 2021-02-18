1735ec8fb989d2b229e02acd4f29a753e0e2de7a
Stephen Gaffney <stephen.gaffney@yale.edu>
2021-02-18 11:07:13 +0000

    Add logger to package init.

--- src/journal_choice/__init__.py
+++ src/journal_choice/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import logging
 from pkg_resources import get_distribution, DistributionNotFound
 
 try:
@@ -10,6 +11,30 @@ except DistributionNotFound:
     __version__ = 'unknown'
 finally:
     del get_distribution, DistributionNotFound
+
+
+# ENSURE DOTENV VARIABLES HAVE LOADED (for gunicorn)
+if not os.getenv('FLASK_CONFIG', ''):
+    from dotenv import load_dotenv, find_dotenv
+    load_dotenv(find_dotenv())
+
+
+def _create_logger():
+    _logger = logging.getLogger(__name__)
+    log_level = getattr(logging, os.environ.get('LOG_LEVEL', 'INFO').upper())
+    _logger.setLevel(log_level)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        fmt='%(asctime)s - %(message)s',
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    ch.setFormatter(formatter)
+    _logger.addHandler(ch)
+    return _logger
+
+
+logger = _create_logger()
 
 DATA_DIR = os.path.sep.join(__file__.split(os.path.sep)[:-3] + ['data'])
 DEMO_DIR = os.path.join(DATA_DIR, 'demo')
