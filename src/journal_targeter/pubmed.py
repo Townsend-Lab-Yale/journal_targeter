e3fde8cbd12e02a5fe6ee52f54123d80644cdec2
Stephen Gaffney <stephen.gaffney@yale.edu>
2021-08-04 10:56:09 +0100

    Get NCBI key from os.environ if present.

--- src/journal_targeter/pubmed.py
+++ src/journal_targeter/pubmed.py
@@ -514,10 +514,15 @@ def load_pubmed_journals(api_key: Union[str, None] = None):
     """Load table of PubMed journals, append extended metadata, drop inactive.
 
     Reload source from NCBI if file doesn't exist.
+
+    Args:
+        api_key: NCBI Entrez API Key (optional). API_KEY in environment will be
+            used as fallback if present.
     """
     if not os.path.exists(paths.PM_MEDLINE_PATH):
         _download_pubmed_reference()
     pm = pd.DataFrame.from_records(_yield_records(paths.PM_MEDLINE_PATH))
+    api_key = api_key if api_key is not None else os.environ.get('API_KEY', None)
     meta = _load_metadata_for_nlmids(pm.nlmid, drop_extra=True, api_key=api_key)
     pm.set_index('nlmid', inplace=True)
     pm = pm.join(meta[meta.is_active], how='inner')  # reduces pm to active only
