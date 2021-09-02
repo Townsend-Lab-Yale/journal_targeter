512910a98951b1eaa81db3e9a637e0cb80a5aca5
Stephen Gaffney <stephen.gaffney@yale.edu>
2021-09-02 14:10:21 +0100

    Put package init in pyscaffold order.

--- src/journal_targeter/__init__.py
+++ src/journal_targeter/__init__.py
@@ -1,8 +1,4 @@
-import os
 import sys
-import logging
-from typing import Union
-
 
 if sys.version_info[:2] >= (3, 8):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
@@ -12,12 +8,17 @@ else:
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = "journal_targeter"
+    dist_name = __name__
     __version__ = version(dist_name)
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del version, PackageNotFoundError
+
+
+import os
+import logging
+from typing import Union
 
 
 def _create_logger(log_level: Union[str, None] = None):
